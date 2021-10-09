7ed3e8a2c31c83af35a260c7e2b497c7ea42bd74
Mathilde-lorrain <69669893+Mathilde-lorrain@users.noreply.github.com>
2021-10-09 10:05:09 +0200

    Update application.yml

--- simple-api/src/main/java/fr/takima/training/simpleapi/controller/DepartmentController.java
+++ simple-api/src/main/java/fr/takima/training/simpleapi/controller/DepartmentController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping(value = "/departments")
 public class DepartmentController {
     private DepartmentService departmentService;
